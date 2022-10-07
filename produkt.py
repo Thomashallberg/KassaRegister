3d64773fc111906d10fcfc9650af996b4fbfe894
Thomashallberg <thomas.hallberg91@outlook.com>
2022-10-07 20:20:32 +0200

    Skelett

---
+++ produkt.py
@@ -0,0 +1,9 @@
+class Product:
+    def __init__(self, name:str, price:float, productid:str ):
+        self.__Name = name
+        self.__Price = price
+        self.__ProductId = productid
+        self.__PriceType = ""
+
+    def GetName(self):
+        return self.__Name
